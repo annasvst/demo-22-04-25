590c9ee82b2e8929d2e1e2863af9f0e62683f3a2
code2career <info@annaatalay.com>
2025-04-22 20:24:16 +0200

    Comment out NewRecipeForm and related imports in NewRecipeForm and page components

--- src/app/recipes/new/page.tsx
+++ src/app/recipes/new/page.tsx
@@ -1,4 +1,4 @@
-import NewRecipeForm from "./NewRecipeForm";
+// import NewRecipeForm from "./NewRecipeForm";
 
 const NewRecipePage = () => {
   
@@ -6,7 +6,7 @@ const NewRecipePage = () => {
     <div className="text-center my-8 max-w-6xl mx-auto">
       <h1 className="text-2xl mb-4">Create New Recipe</h1>
 
-      <NewRecipeForm />
+      {/* <NewRecipeForm /> */}
     </div>
   );
 };
